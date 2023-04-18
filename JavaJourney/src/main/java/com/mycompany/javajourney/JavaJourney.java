d9e652d9c9c0f659fcbfa46650528a8a41c3e27c
Lokesh-Manola <127662523+Lokesh-Manola@users.noreply.github.com>
2023-04-18 21:48:10 +0530

    initial commit

---
+++ JavaJourney/src/main/java/com/mycompany/javajourney/JavaJourney.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javajourney;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class JavaJourney {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
