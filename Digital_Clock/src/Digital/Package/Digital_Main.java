034b7dbe3a02443daf4f6e31c0f0917e48664112
Lokesh-Manola <127662523+Lokesh-Manola@users.noreply.github.com>
2023-06-08 21:01:58 +0530

    my initial commit

---
+++ Digital_Clock/src/Digital/Package/Digital_Main.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Digital.Package;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Digital_Main {
+    public static void main(String args[]){
+        My_Window w=new My_Window();
+    }
+}
